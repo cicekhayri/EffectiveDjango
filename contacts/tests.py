08d7b356da6fb14f4eb95fb26b2627a95e45225d
Hayri Cicek <hello@hayricicek.se>
2014-11-17 16:31:33 +0100

    added tests

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, LiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+from rebar.testing import flatten_to_dict
 
+from contacts import forms
 from contacts.models import Contact
 
 class ContactTests(TestCase):
@@ -45,4 +47,25 @@ class ContactListIntegrationTest(LiveServerTestCase):
 
         self.selenium.find_element_by_id("save_contact").click()
         self.assertEqual(self.selenium.find_elements_by_css_selector('.contact')[-1].text, 'test contact')
+
+class EditContactFormTest(TestCase):
+    def test_mismatch_email_is_invalid(self):
+        form_data = flatten_to_dict(forms.ContactForm())
+        form_data['first_name'] = 'Foo'
+        form_data['last_name'] = 'Bar'
+        form_data['email'] = 'foo@example.com'
+        form_data['confirm_email'] = 'bar@example.com'
+
+        bound_form = forms.ContactForm(data=form_data)
+        self.assertFalse(bound_form.is_valid())
+
+    def test_same_email_is_valid(self):
+        form_data = flatten_to_dict(forms.ContactForm())
+        form_data['first_name'] = 'Foo'
+        form_data['last_name'] = 'Bar'
+        form_data['email'] = 'foo@example.com'
+        form_data['confirm_email'] = 'foo@example.com'
+
+        bound_form = forms.ContactForm(data=form_data)
+        self.assert_(bound_form.is_valid())
 
