4fd14fa5c80424e3a26678fe87e539d2f271a9a4
Hayri Cicek <hayricicek@Hayris-Air.lan>
2014-11-17 11:31:44 +0100

    test_str added

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,16 +1,11 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from contacts.models import Contact
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ContactTests(TestCase):
+    """Contact model tests."""
+
+    def test_str(self):
+        contact = Contact(first_name='Hayri', last_name='Cicek')
+
+        self.assertEquals(str(contact), 'Hayri Cicek', )
