12fcb9b6e76ade9495d676534f4347478b7bba0e
Hayri Cicek <hayricicek@Hayris-Air.lan>
2014-11-17 11:51:03 +0100

    create view added

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+import contacts.views
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -9,6 +11,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'addressbook.views.home', name='home'),
     # url(r'^addressbook/', include('addressbook.foo.urls')),
 
+    url(r'^$', contacts.views.ListContactView.as_view(), name='contacts-list',),
+    url(r'^new$', contacts.views.CreateContactView.as_view(), name='contacts-new',),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
